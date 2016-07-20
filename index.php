5a2931ed1d191bf3c4a4f19afb664753e134a31b
unknown <cunhasilva.w@gmail.com>
2016-07-19 21:58:22 -0300

    Commit da index

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+@User Wagner Cunha
 -->
 <html>
     <head>
@@ -11,8 +9,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
-        echo "Wagner";
+         
         ?>
     </body>
 </html>
